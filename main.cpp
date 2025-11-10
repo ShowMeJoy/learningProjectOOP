6534d04285b1a2d8e84e27d648d191558fabe069
showmejoy <showmejoy@icloud.com>
2025-11-10 16:36:52 +0300

    minor fixes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class Software {
     std::string name_;
@@ -39,15 +40,8 @@ public:
 
     // Геттеры
     const std::string getNetworkAddress() const {return networkAddress_;}
-    int getId()  {return id_;}
-    std::string getSoftwareInfo(int licence) {
-        for (const auto& s : installed_) {
-            if (s.licenseNo() == licence) {
-                return "License №:" + std::to_string(s.licenseNo()) + ", Software: " + s.name();
-            }
-        }
-        return "Software with license " + std::to_string(licence) + " not found";
-    }
+    int getId() const {return id_;}
+    const std::vector<Software>& getInstalled() const {return installed_;}
 
     void addSoftware(Software s) {
         installed_.push_back(s);
@@ -62,27 +56,75 @@ private:
                 
 class User {
     std::string username_;
-    Computer owned_;
+    Computer computer_;
 public:
     User() = default;
-    User (const std::string& name) 
-        : username_(name) {
+    User (const std::string& name, Computer comp) 
+        : username_(name),  computer_(comp) {
         if (name.empty()) throw std::invalid_argument("Empty name");
     }
-    const std::string& name() const {return username_;}
+    const std::string& getName() const {return username_;}
+    const Computer& getComputer() const {return computer_;}
+};
+
+class Inventory {
+    std::vector<User> users_;
+    std::vector<int> licenses_;
+public:
+    // Inventory() = default;
+    // Inventory (const std::vector<User>& users, const std::vector<int>& licenses) 
+    //     : users_(users), licenses_(licenses) {
+    //         if (users.empty() || licenses.empty()) throw std::invalid_argument("Empty list!");
+    //     }
+    void addUser(User user) {
+        users_.push_back(user);
+    }    
+    void addLicenses(int licenses) {
+        licenses_.push_back(licenses);
+    }
+    // Методы для поиска
+    std::vector<std::string> findMatches() const {
+        std::vector<std::string> result;
+        for (const auto& user : users_) {
+            for (const auto& soft : user.getComputer().getInstalled()) {
+                if (std::find(licenses_.begin(), licenses_.end(), soft.licenseNo()) != licenses_.end()) {
+                    result.push_back(user.getName() + " has " + soft.name() + 
+                                    " (license " + std::to_string(soft.licenseNo()) + ")");
+                }
+            }
+        }
+        return result;
+    }
 };
 
 int main () {
-    // Computer pc1("Alice");
-    // pc1.soft.push_back(Software("Windows", 42));
-    // pc1.device = Device("Laptop", 21);
 
-    // if (pc1.device.address() == 21 && pc1.device.device() == "Laptop") {
-    //     std::cout << "It's " << pc1.person.name() 
-    //     << " with Windows 11 with licence №" << pc1.soft[0].licenseNo() << std::endl;
-    // }
     Computer pc1(42, "192.168.7.1");
-    pc1.addSoftware(Software("Browser", 42));
-    std::cout << pc1.getNetworkAddress() << " " << pc1.getId() << std::endl;
-    std::cout << pc1.getSoftwareInfo(42) << '\n' << pc1.getSoftwareInfo(52) << '\n';
+    Computer pc2(21, "192.168.0.1");
+    Computer pc3(33, "192.168.1.3");
+
+    pc1.addSoftware(Software("Browser", 101));
+    pc1.addSoftware(Software("Malware", 202));
+    pc2.addSoftware(Software("Game", 303));
+    pc2.addSoftware(Software("IDE", 404));
+    pc3.addSoftware(Software("Browser", 42));
+    pc3.addSoftware(Software("MediaPlayer", 11));
+
+    User user1("Alice", pc1);
+    User user2("Bob", pc2);
+    User user3("George", pc3);
+
+    Inventory inv;
+
+    inv.addUser(user1);
+    inv.addUser(user2);
+    inv.addUser(user3);
+
+    inv.addLicenses(101);
+    inv.addLicenses(202);
+    inv.addLicenses(303);
+
+    for (auto& line : inv.findMatches()) {
+        std::cout << line << '\n';
+    }
 };
